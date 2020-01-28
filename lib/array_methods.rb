be5db3e8146724f24c56ccc5d77de415396be021
isabel k. lee <hello@isabelklee.co>
2020-01-28 20:46:35 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -28,10 +28,10 @@ end
 
 def find_min_value(array)
   x = array[0]
-  array.length.times do |index|
+  array.length.times { |index|
     if array[index] < x
       x = array[index]
     end
-  end
+  }
 x 
 end 
